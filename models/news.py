a66ed0e0cf12850c1c7c7d820bad9a1f77639764
Lance <54966084+Lance-Tsui@users.noreply.github.com>
2023-04-30 16:36:55 -0400

    news frontend improve

--- models/news.py
+++ models/news.py
@@ -2,8 +2,12 @@ from models import mysql
 
 # Define the User model
 class News:
-    def __init__(self, title):
+    def __init__(self, title, summary, content, publishdate, name):
         self.title = title
+        self.summary = summary
+        self.content = content
+        self.publishdate = publishdate
+        self.name = name
 
     @staticmethod
     def find_news():
@@ -11,5 +15,8 @@ class News:
         cur.execute('SELECT title, summary, content, publishdate, name FROM alu_news A JOIN alu_editor B on A.publishby = B.Id')
         rows = cur.fetchall()
         rows = list(rows)
-        users = rows
-        return users
+        news = []
+        for row in rows:
+            new = News(row[0], row[1], row[2], row[3], row[4])
+            news.append(new)
+        return news

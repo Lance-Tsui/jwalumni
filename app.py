e3d32365ad75ec85177d5d646c64da8273cbbb3d
Lance <54966084+Lance-Tsui@users.noreply.github.com>
2023-05-04 22:13:11 -0400

    favicon.ico

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, Blueprint, render_template
-from models import mysql, connect_to_database
-from models.users import User
-from models.news import News
+from flask import Flask, send_from_directory
+from models import connect_to_database
 from views import views_bp
 
 # Initialize the Flask app
@@ -9,6 +7,10 @@ app = Flask(__name__)
 
 # Connect to the database
 connect_to_database(app)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.root_path, 'static/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 app.register_blueprint(views_bp)
 

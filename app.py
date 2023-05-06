4c4b172b005ee9f4b1e0fc3690bc79ca04d528ad
Lance <54966084+Lance-Tsui@users.noreply.github.com>
2023-05-05 20:54:11 -0400

    multiple css files & put footer to bottom

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory
 from models import connect_to_database
 from views import views_bp
+import os
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -11,6 +12,10 @@ connect_to_database(app)
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(app.root_path, 'static/img/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.context_processor
+def inject_os():
+    return {'os': os}
 
 app.register_blueprint(views_bp)
 

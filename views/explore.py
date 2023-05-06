b2bb9e1da2baf092a4d882d2e85f700bda06a4a3
Lance <54966084+Lance-Tsui@users.noreply.github.com>
2023-05-05 22:29:58 -0400

    filter function searchbox

--- views/explore.py
+++ views/explore.py
@@ -1,9 +1,17 @@
-from flask import render_template
+from flask import render_template, request
 from . import views_bp
 from models.users import User
 
 # Define the about page route
 @views_bp.route('/explore')
 def explore():
+    query = request.args.get('q')
     users = User.find_all()
+    result = []
+    if query:
+        for i in users:
+            if query.lower() in i.title.lower():
+                result.append(i)
+        users = result
+
     return render_template('explore.html', users=users)

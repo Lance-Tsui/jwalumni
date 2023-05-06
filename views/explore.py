a69f58b589087e53ff45c3747e7e34b5b9aa67a0
Lance <54966084+Lance-Tsui@users.noreply.github.com>
2023-05-06 00:28:34 -0400

    search and dropdown filter function

--- views/explore.py
+++ views/explore.py
@@ -1,17 +1,31 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from . import views_bp
 from models.users import User
 
 # Define the about page route
 @views_bp.route('/explore')
 def explore():
-    query = request.args.get('q')
     users = User.find_all()
-    result = []
+    return render_template('explore.html', users=users)
+
+@views_bp.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+    drop = request.args.get('d')
+    users = User.find_all()
     if query:
+        result = []
         for i in users:
             if query.lower() in i.title.lower():
                 result.append(i)
         users = result
 
-    return render_template('explore.html', users=users)
+    if drop:
+        result = []
+        for i in users:
+            if i.region.lower() == drop.lower():
+                result.append(i)
+        users = result
+
+    # 只返回用户列表的 JSON 数据，不返回整个 HTML 页面
+    return jsonify(users=[{'title': user.title, 'region': user.region} for user in users])
